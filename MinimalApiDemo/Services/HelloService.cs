850c0151392a2a1bfd59d5737d60f0a2d454cd2b
Diego A. Dias <dtdias@icatuseguros.com.br>
2021-09-25 18:54:58 -0300

    Add project files.

---
+++ MinimalApiDemo/Services/HelloService.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace MinimalApiDemo.Services
+{
+
+    public interface IHelloService
+    {
+        string Hello(ClaimsPrincipal user, bool isHappy);
+    }
+
+    public class HelloService : IHelloService
+    {
+        public string Hello(ClaimsPrincipal user, bool isHappy)
+        {
+            var hello = $"Hello {user.Identity.Name}";
+
+            if (isHappy)
+                return $"{hello}, you seem to be happy today";
+            return hello;
+        }
+    }
+}
